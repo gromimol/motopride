6f9b70618e4adf74fa5ccfd2b8fcdfa06a1a0bcc
Grom <gromimol@ukr.net>
2019-09-18 12:56:09 +0300

    Десктоп и адаптив

--- js/common.js
+++ js/common.js
@@ -1,26 +1,9 @@
+
+// Оптимизация для youtube-видео
+
+
+
 $(document).ready(function () {
-
-	// click on scroll icon
-	$('.scroll-down').on('click',function (e) {
-		e.preventDefault();
-
-		$('.pages').addClass('active');
-		$('.page').eq(0).addClass('active');
-	})
-
-	// menu
-	$('.menu').on('click', 'a', function (e) {
-		e.preventDefault();
-
-		var pageNumber = $(this).attr('data-page');
-
-		$('.menu a').removeClass('active');
-		$(this).addClass('active');
-
-		$('.page').removeClass('active');
-		
-		$('#' + pageNumber).addClass('active');
-	})
 
 	// popup
 	$('.js-popup-link').on('click',function (e) {
@@ -33,6 +16,7 @@ $(document).ready(function () {
 		$('#' + popupTarget).addClass('active')
 	});
 
+
 	// Закрываем попап
 	$('.back').on('click',function (e) {
 		e.preventDefault();
@@ -40,6 +24,38 @@ $(document).ready(function () {
 		$('#overlay').removeClass('active');
 		$('.popup').removeClass('active');
 	});
+
+	// click on scroll icon
+	$('.scroll-down').on('click',function (e) {
+		e.preventDefault();
+
+		$('.first-screen').addClass('hidden')
+		$('.pages').addClass('active');
+		$('.page').eq(0).addClass('active');
+	});
+
+	$('.logo').on('click',function () {
+		$('.first-screen').removeClass('hidden')
+		$('.pages').removeClass('active');
+	});
+
+	// menu
+	$('.menu').on('click', 'a', function (e) {
+		e.preventDefault();
+
+		var pageNumber = $(this).attr('data-page');
+
+		$('.menu a').removeClass('active');
+		$(this).addClass('active');
+
+		$('.page.bg').removeClass('bg');
+		$('.page.active').removeClass('active').addClass('bg');	
+
+		$('#' + pageNumber).addClass('active');
+	});
+
+
+	
 
 	// Просто демонстрация попапа после submit
 	$('.js-submit').on('click',function (e) {
@@ -61,8 +77,11 @@ $(document).ready(function () {
 		$('.news-photo').attr('src', imgSrc);
 	})
 
-	$(".news-list").niceScroll();
-	$(".news-body").niceScroll();
+	if($(window).width() > 1360){
+		$(".news-list").niceScroll();
+		$(".news-body").niceScroll();
+	}
+	
 
 	// slider
 	$('.photo-group').slick({
@@ -70,7 +89,29 @@ $(document).ready(function () {
 		slidesToShow: 3,
 		slidesToScroll: 3,
 		prevArrow: '<span class="prev-arrow"><svg id="SVGDoc1" width="40" height="74" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:avocode="https://avocode.com/" viewBox="0 0 40 74"><g><g><title>Line 15</title><path d="M2.64468,36.32245l35.35532,35.35527" fill-opacity="0" fill="currentColor" stroke-dashoffset="0" stroke-linejoin="miter" stroke-linecap="butt" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="5"></path></g><g><title>Line 14</title><path d="M38,2.32227l-35.6777,36.67773" fill-opacity="0" fill="currentColor" stroke-dashoffset="0" stroke-linejoin="miter" stroke-linecap="butt" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="5"></path></g><g><title>Group 10</title><g><title>Line 14</title><path d="M38,2.32227l-35.6777,36.67773" fill-opacity="0" fill="currentColor" stroke-dashoffset="0" stroke-linejoin="miter" stroke-linecap="butt" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="5"></path></g><g><title>Line 15</title><path d="M2.64468,36.32245l35.35532,35.35527" fill-opacity="0" fill="currentColor" stroke-dashoffset="0" stroke-linejoin="miter" stroke-linecap="butt" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="5"></path></g></g></g></svg></span>',
-		nextArrow: '<span class="next-arrow"><svg id="SVGDoc2" width="40" height="74" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:avocode="https://avocode.com/" viewBox="0 0 40 74"><g><g><title>Line 4</title><path d="M37.35531,36.3223l-35.35534,35.35528" fill-opacity="0" fill="currentColor" stroke-dashoffset="0" stroke-linejoin="miter" stroke-linecap="butt" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="5"></path></g><g><path d="M2,2.32239l35.67773,36.67773" fill-opacity="0" fill="currentColor" stroke-dashoffset="0" stroke-linejoin="miter" stroke-linecap="butt" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="5"></path></g><g><g><path d="M2,2.32239l35.67773,36.67773" fill-opacity="0" fill="currentColor" stroke-dashoffset="0" stroke-linejoin="miter" stroke-linecap="butt" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="5"></path></g><g><title>Line 4</title><path d="M37.35531,36.3223l-35.35534,35.35528" fill-opacity="0" fill="currentColor" stroke-dashoffset="0" stroke-linejoin="miter" stroke-linecap="butt" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="5"></path></g></g></g></svg></span>'
+		nextArrow: '<span class="next-arrow"><svg id="SVGDoc2" width="40" height="74" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:avocode="https://avocode.com/" viewBox="0 0 40 74"><g><g><title>Line 4</title><path d="M37.35531,36.3223l-35.35534,35.35528" fill-opacity="0" fill="currentColor" stroke-dashoffset="0" stroke-linejoin="miter" stroke-linecap="butt" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="5"></path></g><g><path d="M2,2.32239l35.67773,36.67773" fill-opacity="0" fill="currentColor" stroke-dashoffset="0" stroke-linejoin="miter" stroke-linecap="butt" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="5"></path></g><g><g><path d="M2,2.32239l35.67773,36.67773" fill-opacity="0" fill="currentColor" stroke-dashoffset="0" stroke-linejoin="miter" stroke-linecap="butt" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="5"></path></g><g><title>Line 4</title><path d="M37.35531,36.3223l-35.35534,35.35528" fill-opacity="0" fill="currentColor" stroke-dashoffset="0" stroke-linejoin="miter" stroke-linecap="butt" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="5"></path></g></g></g></svg></span>',
+		responsive: [
+			{
+				breakpoint: 1023,
+				settings: {
+					rows: 1
+				}
+			},
+			{
+				breakpoint: 767,
+				settings: {
+					rows: 1,
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 400,
+				settings: {
+					rows: 1,
+					slidesToShow: 1
+				}
+			}
+		]
 	});
 
 	$('.video-slider').slick({
@@ -79,7 +120,16 @@ $(document).ready(function () {
 		slidesToShow: 3,
 		centerMode: true,
 		prevArrow: '<span class="prev-arrow"><svg id="SVGDoc1" width="40" height="74" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:avocode="https://avocode.com/" viewBox="0 0 40 74"><g><g><title>Line 15</title><path d="M2.64468,36.32245l35.35532,35.35527" fill-opacity="0" fill="currentColor" stroke-dashoffset="0" stroke-linejoin="miter" stroke-linecap="butt" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="5"></path></g><g><title>Line 14</title><path d="M38,2.32227l-35.6777,36.67773" fill-opacity="0" fill="currentColor" stroke-dashoffset="0" stroke-linejoin="miter" stroke-linecap="butt" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="5"></path></g><g><title>Group 10</title><g><title>Line 14</title><path d="M38,2.32227l-35.6777,36.67773" fill-opacity="0" fill="currentColor" stroke-dashoffset="0" stroke-linejoin="miter" stroke-linecap="butt" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="5"></path></g><g><title>Line 15</title><path d="M2.64468,36.32245l35.35532,35.35527" fill-opacity="0" fill="currentColor" stroke-dashoffset="0" stroke-linejoin="miter" stroke-linecap="butt" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="5"></path></g></g></g></svg></span>',
-		nextArrow: '<span class="next-arrow"><svg id="SVGDoc2" width="40" height="74" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:avocode="https://avocode.com/" viewBox="0 0 40 74"><g><g><title>Line 4</title><path d="M37.35531,36.3223l-35.35534,35.35528" fill-opacity="0" fill="currentColor" stroke-dashoffset="0" stroke-linejoin="miter" stroke-linecap="butt" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="5"></path></g><g><path d="M2,2.32239l35.67773,36.67773" fill-opacity="0" fill="currentColor" stroke-dashoffset="0" stroke-linejoin="miter" stroke-linecap="butt" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="5"></path></g><g><g><path d="M2,2.32239l35.67773,36.67773" fill-opacity="0" fill="currentColor" stroke-dashoffset="0" stroke-linejoin="miter" stroke-linecap="butt" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="5"></path></g><g><title>Line 4</title><path d="M37.35531,36.3223l-35.35534,35.35528" fill-opacity="0" fill="currentColor" stroke-dashoffset="0" stroke-linejoin="miter" stroke-linecap="butt" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="5"></path></g></g></g></svg></span>'
+		nextArrow: '<span class="next-arrow"><svg id="SVGDoc2" width="40" height="74" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:avocode="https://avocode.com/" viewBox="0 0 40 74"><g><g><title>Line 4</title><path d="M37.35531,36.3223l-35.35534,35.35528" fill-opacity="0" fill="currentColor" stroke-dashoffset="0" stroke-linejoin="miter" stroke-linecap="butt" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="5"></path></g><g><path d="M2,2.32239l35.67773,36.67773" fill-opacity="0" fill="currentColor" stroke-dashoffset="0" stroke-linejoin="miter" stroke-linecap="butt" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="5"></path></g><g><g><path d="M2,2.32239l35.67773,36.67773" fill-opacity="0" fill="currentColor" stroke-dashoffset="0" stroke-linejoin="miter" stroke-linecap="butt" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="5"></path></g><g><title>Line 4</title><path d="M37.35531,36.3223l-35.35534,35.35528" fill-opacity="0" fill="currentColor" stroke-dashoffset="0" stroke-linejoin="miter" stroke-linecap="butt" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="5"></path></g></g></g></svg></span>',
+		responsive: [
+			{
+				breakpoint: 1360,
+				settings: {
+					slidesToShow: 1,
+					centerMode: false
+				}
+			}
+		]
 
 	});
 
@@ -95,7 +145,16 @@ $(document).ready(function () {
 		initialSlide: 1,
 		centerMode: true,
 		prevArrow: '<span class="prev-arrow"><svg id="SVGDoc4" width="40" height="74" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:avocode="https://avocode.com/" viewBox="0 0 40 74"><g><g><title>Line 15</title><path d="M2.64468,36.32245l35.35532,35.35527" fill-opacity="0" fill="currentColor" stroke-dashoffset="0" stroke-linejoin="miter" stroke-linecap="butt" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="5"></path></g><g><title>Line 14</title><path d="M38,2.32227l-35.6777,36.67773" fill-opacity="0" fill="currentColor" stroke-dashoffset="0" stroke-linejoin="miter" stroke-linecap="butt" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="5"></path></g><g><title>Group 10</title><g><title>Line 14</title><path d="M38,2.32227l-35.6777,36.67773" fill-opacity="0" fill="currentColor" stroke-dashoffset="0" stroke-linejoin="miter" stroke-linecap="butt" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="5"></path></g><g><title>Line 15</title><path d="M2.64468,36.32245l35.35532,35.35527" fill-opacity="0" fill="currentColor" stroke-dashoffset="0" stroke-linejoin="miter" stroke-linecap="butt" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="5"></path></g></g></g></svg></span>',
-		nextArrow: '<span class="next-arrow"><svg id="SVGDoc5" width="40" height="74" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:avocode="https://avocode.com/" viewBox="0 0 40 74"><g><g><title>Line 4</title><path d="M37.35531,36.3223l-35.35534,35.35528" fill-opacity="0" fill="currentColor" stroke-dashoffset="0" stroke-linejoin="miter" stroke-linecap="butt" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="5"></path></g><g><path d="M2,2.32239l35.67773,36.67773" fill-opacity="0" fill="currentColor" stroke-dashoffset="0" stroke-linejoin="miter" stroke-linecap="butt" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="5"></path></g><g><g><path d="M2,2.32239l35.67773,36.67773" fill-opacity="0" fill="currentColor" stroke-dashoffset="0" stroke-linejoin="miter" stroke-linecap="butt" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="5"></path></g><g><title>Line 4</title><path d="M37.35531,36.3223l-35.35534,35.35528" fill-opacity="0" fill="currentColor" stroke-dashoffset="0" stroke-linejoin="miter" stroke-linecap="butt" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="5"></path></g></g></g></svg></span>'
+		nextArrow: '<span class="next-arrow"><svg id="SVGDoc5" width="40" height="74" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:avocode="https://avocode.com/" viewBox="0 0 40 74"><g><g><title>Line 4</title><path d="M37.35531,36.3223l-35.35534,35.35528" fill-opacity="0" fill="currentColor" stroke-dashoffset="0" stroke-linejoin="miter" stroke-linecap="butt" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="5"></path></g><g><path d="M2,2.32239l35.67773,36.67773" fill-opacity="0" fill="currentColor" stroke-dashoffset="0" stroke-linejoin="miter" stroke-linecap="butt" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="5"></path></g><g><g><path d="M2,2.32239l35.67773,36.67773" fill-opacity="0" fill="currentColor" stroke-dashoffset="0" stroke-linejoin="miter" stroke-linecap="butt" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="5"></path></g><g><title>Line 4</title><path d="M37.35531,36.3223l-35.35534,35.35528" fill-opacity="0" fill="currentColor" stroke-dashoffset="0" stroke-linejoin="miter" stroke-linecap="butt" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="5"></path></g></g></g></svg></span>',
+		responsive: [
+			{
+				breakpoint: 1360,
+				settings: {
+					slidesToShow: 1,
+					centerMode: false
+				}
+			}
+		]
 
 	});
 
